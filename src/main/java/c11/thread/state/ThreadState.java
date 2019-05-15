f60ee08ea184291153e5eb39dd4f57d967a6488a
jijunshi <jijunshi@creditease.cn>
2019-05-15 17:45:47 +0800

    增加使用reentrantlock的用例。

--- src/main/java/c11/thread/state/ThreadState.java
+++ src/main/java/c11/thread/state/ThreadState.java
@@ -5,6 +5,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
 
 /*
 *   演示java thread的各个不同的状态以及状态的转化；
@@ -137,6 +138,49 @@ public class ThreadState {
         TimeUnit.SECONDS.sleep(1);
     }
 
+    //WAITING 通过ReentrantLock;
+    //注意： ReentrantLock 是基于AQS实现的； AQS底层是使用LockSupport实现线程的操作的；所以这个测试中线程的状态是WAITING， 而不是BLOCKED
+    @Test
+    public void testThreadState_2_WAITING() throws InterruptedException {
+        final ReentrantLock lock = new ReentrantLock();
+
+        Thread thread1 = new Thread(new Runnable() {
+            public void run() {
+                try {
+                    lock.lock();
+                    try {
+                        //注意：执行完这句话后，thread1 会进入TIMED_WAITING状态；
+                        TimeUnit.SECONDS.sleep(3);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }finally {
+                    lock.unlock();
+                }
+            }
+        });
+        thread1.start();
+
+        //等待1秒钟，此时thread1肯定正在拿着 lock对象的锁；
+        TimeUnit.SECONDS.sleep(1);
+        Thread thread2 = new Thread(new Runnable() {
+            public void run() {
+                try{
+                    lock.lock();
+                    //do nothing
+                }finally {
+                    lock.unlock();
+                }
+            }
+        });
+        thread2.start();
+
+        TimeUnit.SECONDS.sleep(1);
+        //此时thread2肯定正在等待 lock对象的锁，因为锁被thread1 占有着；
+        PrintUtils.print(thread2.getState().toString());
+        Assert.assertTrue(thread2.getState() == Thread.State.WAITING);
+    }
+
     //BLOCKED
     @Test
     public void testThreadState_BLOCKED() throws InterruptedException {
@@ -172,4 +216,6 @@ public class ThreadState {
         PrintUtils.print(thread2.getState().toString());
         Assert.assertTrue(thread2.getState() == Thread.State.BLOCKED);
     }
+
+
 }
