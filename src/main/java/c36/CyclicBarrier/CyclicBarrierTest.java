346a580dd94d2c953fec731f72439cf23d24ba81
Administrator <5080053@qq.com>
2019-06-02 10:15:03 +0800

    aa

--- src/main/java/c36/CyclicBarrier/CyclicBarrierTest.java
+++ src/main/java/c36/CyclicBarrier/CyclicBarrierTest.java
@@ -8,19 +8,21 @@ import java.util.Random;
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 
-public class Solver {
+public class CyclicBarrierTest {
     int N; //标识选手的个数
-    float[][] data; //记录每个选手每个阶段的用时
+    int[][] data; //记录每个选手每个阶段的用时
     final CyclicBarrier barrier;
     volatile boolean done = false;
 
     public static void main(String[] args) throws InterruptedException {
-        new Solver(10);
+        new CyclicBarrierTest(10);
     }
-    public Solver(int n) throws InterruptedException {
-        data = new float[n][3]; //n * 3 的二维数组
+
+
+    public CyclicBarrierTest(int n) throws InterruptedException {
+        data = new int[n][3]; //n * 3 的二维数组
         N = n;
-        Runnable barrierAction = new SumCompter();
+        Runnable barrierAction = new SumComputer();
         //
         barrier = new CyclicBarrier(N, barrierAction);
 
@@ -36,10 +38,10 @@ public class Solver {
         }
     }
 
-    class SumCompter implements Runnable{
+    class SumComputer implements Runnable{
         private int round = 0 ;
         public void run() {
-            float sum = 0;
+            int sum = 0;
             for (int i = 0; i<10; ++i){
                 sum += data[i][round];
             }
@@ -47,6 +49,7 @@ public class Solver {
             round ++;
         }
     }
+
     class Player implements Runnable {
         int no;  //表示是第几名选手;
         int round;  //表示当前比赛是第几轮;一共3轮比赛;
@@ -58,7 +61,7 @@ public class Solver {
         public void run() {
             while (round < 3) {
                 Random r = new Random();
-                float timeSpend = r.nextFloat();
+                int timeSpend = r.nextInt(10);
                 PrintUtils.print("当前选手:" + this.no + ", 当前round:" + this.round + ", timeSpend: " + timeSpend);
                 //把话费的事件保存到数据中去
                 data[no][round] = timeSpend;
