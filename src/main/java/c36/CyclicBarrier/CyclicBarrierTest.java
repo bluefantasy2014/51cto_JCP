3a33a1536a66697ca46e71517114cfe0fa4c0a45
Administrator <5080053@qq.com>
2019-06-07 22:27:58 +0800

    aa

--- src/main/java/c36/CyclicBarrier/CyclicBarrierTest.java
+++ src/main/java/c36/CyclicBarrier/CyclicBarrierTest.java
@@ -9,60 +9,74 @@ import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 
 public class CyclicBarrierTest {
-    int N; //标识选手的个数
+    int playerCount; //标识选手的个数
+    int round ; //比赛一共有几轮
     int[][] data; //记录每个选手每个阶段的用时
-    final CyclicBarrier barrier;
-    volatile boolean done = false;
+    final CyclicBarrier barrier; //
 
     public static void main(String[] args) throws InterruptedException {
-        new CyclicBarrierTest(10);
+        CyclicBarrierTest test = new CyclicBarrierTest(10,3);
+        test.startMatch();
     }
 
+    public CyclicBarrierTest(int playerCount, int round) {
+       this.playerCount = playerCount;
+       this.round = round;
+       this.data = new int[playerCount][round];
+       this.barrier = new CyclicBarrier(this.playerCount, new ScoreComputer(playerCount));
+    }
 
-    public CyclicBarrierTest(int n) throws InterruptedException {
-        data = new int[n][3]; //n * 3 的二维数组
-        N = n;
-        Runnable barrierAction = new SumComputer();
-        //
-        barrier = new CyclicBarrier(N, barrierAction);
-
-        List<Thread> threads = new ArrayList<Thread>(N);
-        for (int i = 0; i < N; i++) {
-            Thread thread = new Thread(new Player(i,0));
+    public void startMatch() throws InterruptedException {
+        //为每个Player对象创建一个单独的线程并启动
+        List<Thread> threads = new ArrayList<Thread>(playerCount);
+        for (int i = 0; i < playerCount; i++) {
+            Thread thread = new Thread(new Player(i,this.round));
             threads.add(thread);
             thread.start();
         }
-        // wait until done
+        //等待所有线程执行完毕
         for (Thread thread : threads) {
             thread.join();
         }
     }
 
-    class SumComputer implements Runnable{
-        private int round = 0 ;
+    //计算每个Player比赛耗时的类 (barrierAction)
+    class ScoreComputer implements Runnable{
+        private int round = 0 ;  //表示当前是比赛的第几轮
+        private int [] scores ;  //记录每个Player的总耗时
+
+        public ScoreComputer(int playerCount){
+            this.scores = new int[playerCount];
+        }
+
+       //会计算当前轮比赛后每个Player的总的耗时
         public void run() {
-            int sum = 0;
-            for (int i = 0; i<10; ++i){
-                sum += data[i][round];
+            for (int i = 0; i<data.length; ++i){
+                if (round == 0){ //如果是第1轮，不用累加
+                   this.scores[i] = data[i][round];
+                }else { //如果是大于第一轮，需要累加
+                   this.scores[i]  += data[i][round];
+                }
+                PrintUtils.print(" ScoreComputer 当前round : " + round + "，当前Player：" + i + ", 总用时: " + this.scores[i] );
             }
-            PrintUtils.print("当前round : " + round + ", 平均用时: " + sum/10);
             round ++;
         }
     }
 
     class Player implements Runnable {
         int no;  //表示是第几名选手;
-        int round;  //表示当前比赛是第几轮;一共3轮比赛;
-        Player(int no, int round) {
+        int round = 0;  //表示当前比赛是第几轮;
+        int totalRound;  //表示总的轮数;
+        Player(int no,int totalRound) {
             this.no = no;
-            this.round = round;
+            this.totalRound = totalRound;
         }
 
         public void run() {
-            while (round < 3) {
+            while (round < totalRound) {
                 Random r = new Random();
                 int timeSpend = r.nextInt(10);
-                PrintUtils.print("当前选手:" + this.no + ", 当前round:" + this.round + ", timeSpend: " + timeSpend);
+                PrintUtils.print(" Player 当前选手:" + this.no + ", 当前round:" + this.round + ", timeSpend: " + timeSpend);
                 //把话费的事件保存到数据中去
                 data[no][round] = timeSpend;
 
@@ -74,7 +88,6 @@ public class CyclicBarrierTest {
                 } catch (BrokenBarrierException ex) {
                     return;
                 }
-
                 round ++;
             }
         }
